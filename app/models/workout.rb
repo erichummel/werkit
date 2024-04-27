c6666f631a77d8bfabead08dcd8deafae89c09cc
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 18:20:43 -0400

    swap for a workouts scaffold

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,2 @@
 class Workout < ApplicationRecord
-  belongs_to :user
-  has_one_attached :data
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_211509) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_221836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,13 +38,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_211509) do
   end
 
   create_table "workouts", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
   add_foreign_key "sessions", "users"
   add_foreign_key "sign_in_tokens", "users"
-  add_foreign_key "workouts", "users"
 end
