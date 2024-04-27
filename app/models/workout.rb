a7a290db67abb1f0e301b756947d4baa599106cc
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 16:55:08 -0400

    tests pass

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,4 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_one_attached :data
 end
--- db/migrate/20240427172804_create_workouts.rb
+++ db/migrate/20240427172804_create_workouts.rb
@@ -2,7 +2,7 @@ class CreateWorkouts < ActiveRecord::Migration[7.1]
   def change
     create_table :workouts do |t|
       t.references :user, null: false, foreign_key: true
-      t.timestamp :worked_out_at
+
       t.timestamps
     end
   end
