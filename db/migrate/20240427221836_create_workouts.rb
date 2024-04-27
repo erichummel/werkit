ba179703483d8e106e99d2206a2899854499e1c3
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 18:20:43 -0400

    swap for a workouts scaffold

--- db/migrate/20240427221836_create_workouts.rb
+++ db/migrate/20240427221836_create_workouts.rb
@@ -1,7 +1,6 @@
 class CreateWorkouts < ActiveRecord::Migration[7.1]
   def change
     create_table :workouts do |t|
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
