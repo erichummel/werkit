495b885b1881cc52b84536172b80ecb0dd0a7bf6
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 16:55:08 -0400

    tests pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_171401) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_172804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_171401) do
 
   create_table "workouts", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.datetime "worked_out_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_workouts_on_user_id"
