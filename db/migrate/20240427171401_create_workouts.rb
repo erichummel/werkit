c68c39de268d634ab3cc906528c8816af6705c39
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 13:21:05 -0400

    workout model skeleton

---
+++ db/migrate/20240427171401_create_workouts.rb
@@ -0,0 +1,9 @@
+class CreateWorkouts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :workouts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.timestamp :worked_out_at
+      t.timestamps
+    end
+  end
+end
