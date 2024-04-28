a9d1e57aa8be3c765caabbca8801c251f0b388ef
Eric Hummel <eric.hummel@gmail.com>
2024-04-28 16:05:04 -0400

    ditch map service experiment

--- app/services/map_service.rb
+++
@@ -1,9 +0,0 @@
-class MapService
-  def client
-    @client ||= OpenStreetMap::Client.new
-  end
-
-  def get_waypoint(latitude, longitude)
-    client.reverse(lat: latitude, lon: longitude, format: 'json')
-  end
-end
