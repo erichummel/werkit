60ba20d2c7bb53b8993d5a8947e521d54ffc760b
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 22:56:25 -0400

    wip openstreet map stuff some other stuff

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,12 @@
 class Workout < ApplicationRecord
   has_one_attached :data_file
   belongs_to :user
+
+  def waypoints
+    parsed_data["workouts"].first["route"]
+  end
+
+  def parsed_data
+    @parsed_data ||= JSON.parse(data_file.download)["data"]
+  end
 end
