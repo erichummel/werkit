885c00bc0aadb19439920764460253302c237d46
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 20:08:31 -0400

    basic workout data upload

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,4 @@
 class Workout < ApplicationRecord
+  has_one_attached :data_file
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_222322) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_223006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,10 +68,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_222322) do
   create_table "workouts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "sessions", "users"
   add_foreign_key "sign_in_tokens", "users"
+  add_foreign_key "workouts", "users"
 end
