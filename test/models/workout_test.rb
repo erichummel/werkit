62af29650ca91f3b6402b999220fb528a45a0039
Eric Hummel <eric.hummel@gmail.com>
2024-04-27 22:56:25 -0400

    wip openstreet map stuff some other stuff

--- test/models/workout_test.rb
+++ test/models/workout_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class WorkoutTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "parses uploaded data files" do
+    workout = workouts(:one)
+    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
+    assert_equal(10, workout.waypoints.size)
+  end
 end
