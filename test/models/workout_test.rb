4f981123a596831a59b8ec704848d8729fb4eb9f
Eric Hummel <eric.hummel@gmail.com>
2024-06-01 16:26:24 -0400

    Anonymizer (#6)
    * WIP readme
    
    * WIP anonymizer
    
    * anonymized workout
    
    * tweak to test
    
    * update readme

--- test/models/workout_test.rb
+++ test/models/workout_test.rb
@@ -17,13 +17,33 @@ class WorkoutTest < ActiveSupport::TestCase
     workout = workouts(:one)
     workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
 
-    assert_equal([40.42072301602432, -74.78835592118314], workout.start)
-    assert_equal([40.420707164840294, -74.78862562857125], workout.finish)
+    assert_equal([0, 0], workout.start)
+    assert_equal([0, -0.0002697073881], workout.finish)
   end
 
   test "returns stats for the workout" do
     workout = workouts(:one)
     workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
     assert_equal("9.2 mi", workout.distance)
+  end
+
+  test "anonymizes a workout" do
+    workout = workouts(:one)
+    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
+    workout.anonymize!(Workout::OUTBACK_LATITUDE, Workout::OUTBACK_LONGITUDE)
+
+    assert_not_equal([0, 0], workout.start)
+    assert_not_equal([0, -0.0002697073881], workout.finish)
+
+    sample_waypoint = workout.waypoints.sample
+    assert_operator(
+      Workout.haversine_distance([sample_waypoint.latitude, sample_waypoint.longitude], [0, 0]),
+      :>,
+      1000)
+  end
+
+  test "haversine distance" do
+    distance = Workout.haversine_distance([0, 0], [1, 1])
+    assert_equal(97.71, distance.round(2))
   end
 end
