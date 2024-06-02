b206876935803cd45e9ab909d62c343787231931
Eric Hummel <eric.hummel@gmail.com>
2024-06-01 22:00:03 -0400

    Started at ended at (#5)
    * WIP readme
    
    * WIP anonymizer
    
    * anonymized workout
    
    * set started_at and ended_at in the database from the json
    
    * fix artifacts of a bad merge
    
    * fix bug in anonymization spec

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,12 +4,18 @@ class Workout < ApplicationRecord
 
   attr_accessor :anonymize
 
+  after_commit :set_started_at_and_ended_at, on: [:create, :update]
+
   # TODO: there's a lot of presentation logic in this model. something should be done about that
 
   OUTBACK_LATITUDE = -25.751525
   OUTBACK_LONGITUDE = 134.1065540
   EVEREST_LATITUDE = 27.98789
   EVEREST_LONGITUDE = 86.92502
+
+  def self.haversine_distance(point1, point2)
+    Geocoder::Calculations.distance_between(point1, point2)
+  end
 
   def base
     parsed_data["workouts"].first
@@ -48,10 +54,12 @@ class Workout < ApplicationRecord
   end
 
   def parsed_data
-    @parsed_data ||= parsed_file["data"]
+    # @parsed_data ||= parsed_file["data"]
+    parsed_file["data"]
   end
 
   def parsed_file
+    return {} unless data_file.attached?
     JSON.parse(data_file.download)
   end
 
@@ -150,7 +158,11 @@ class Workout < ApplicationRecord
     }).merge(waypoints_attributes)
   end
 
-  def self.haversine_distance(point1, point2)
-    Geocoder::Calculations.distance_between(point1, point2)
+  private
+
+  def set_started_at_and_ended_at
+    return true unless data_file.attached?
+    self.started_at = start_time
+    self.ended_at = end_time
   end
 end
--- test/models/workout_test.rb
+++ test/models/workout_test.rb
@@ -3,35 +3,48 @@ require "test_helper"
 class WorkoutTest < ActiveSupport::TestCase
   test "parses uploaded data files" do
     workout = workouts(:one)
-    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
+    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycling_workout.json")), filename: "cycling_workout.json")
     assert_equal(10, workout.waypoints.size)
+  end
+
+  test "sets started_at and ended_at from the data file" do
+    workout = workouts(:one)
+    workout.data_file.attach(
+      io: File.open(Rails.root.join("test", "fixtures", "files", "cycling_workout.json")),
+      filename: "cycling_workout.json",
+      content_type: "application/json",
+      identify: false)
+
+    assert_equal("2024-04-27 17:53:50 UTC", workout.started_at.to_s)
+    assert_equal("2024-04-27 18:49:36 UTC", workout.ended_at.to_s)
   end
 
   test "returns four corners of the route" do
     workout = workouts(:one)
-    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
+    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycling_workout.json")), filename: "cycling_workout.json")
     assert_equal(4, workout.bounding_box.size)
   end
 
   test "returns the start and end points of the route" do
     workout = workouts(:one)
-    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
+    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycling_workout.json")), filename: "cycling_workout.json")
 
     assert_equal([0, 0], workout.start)
-    assert_equal([0, -0.0002697073881], workout.finish)
+    assert_equal([-1.5851184024029408e-05, -0.00026970738811371575], workout.finish)
   end
 
   test "returns stats for the workout" do
     workout = workouts(:one)
-    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
+    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycling_workout.json")), filename: "cycling_workout.json")
     assert_equal("9.2 mi", workout.distance)
   end
 
   test "anonymizes a workout" do
     workout = workouts(:one)
-    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycle_workout.json")), filename: "cycle_workout.json")
-    workout.anonymize!(Workout::OUTBACK_LATITUDE, Workout::OUTBACK_LONGITUDE)
 
+    workout.data_file.attach(io: File.open(Rails.root.join("test", "fixtures", "files", "cycling_workout.json")), filename: "cycling_workout.json")
+    workout.anonymize!(Workout::OUTBACK_LATITUDE, Workout::OUTBACK_LONGITUDE)
+    workout = Workout.find(workout.id)
     assert_not_equal([0, 0], workout.start)
     assert_not_equal([0, -0.0002697073881], workout.finish)
 
