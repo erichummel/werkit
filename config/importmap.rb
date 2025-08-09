4ce0981af492557d16b54dde06ea8eb30f4030b1
Eric Hummel <eric.hummel@gmail.com>
2025-08-09 17:21:10 -0400

    Revert "failed attempt at getting plotly to work"
    This reverts commit 7e030defddb129ea3cc58a3491694fca53621cb8.

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,8 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "leaflet" # @1.9.4
 pin "leaflet-providers" # @2.0.0
-pin "plotly" # @1.0.6
-pin "buffer" # @2.1.0
-pin "http" # @2.1.0
-pin "https" # @2.1.0
-pin "url" # @2.1.0
